ca67d377e8576eb75dddaf258844303fc9f75466
ashishsharma2885 <ashishsharma60141@gmail.com>
2026-09-16 20:23:39 +0530

    Add Python course basics

---
+++ 01-HelloWorld/main.py
@@ -0,0 +1,1 @@
+print("Hello World we are learning python")
---
+++ 02-commentAndVariable/comment.py
@@ -0,0 +1,15 @@
+#Hello World we are learning python This is single line comment 
+
+"""This is multiline comment"""
+
+# learing variable
+sher = "Ashish Sharma"
+
+#pascal caase 
+AshishSharma = "Ashish Sharma"
+
+#camel case
+ashishSharmaa = "ashishSharma"
+
+#snaake case
+ashish_sharma = "ashish_Sharma"
