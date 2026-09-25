48fd3c5c14dbacbb7dd33ff0fda8a0f35f5613c6
ashishsharma2885 <ashishsharma60141@gmail.com>
2026-09-25 10:07:05 +0530

    learning input and output

---
+++ 06-InputAndoutput/input.py
@@ -0,0 +1,12 @@
+#input and output in python
+
+# print("Hello how are you")
+
+# name = "Ashish"
+# age = 24
+# print(name, age)
+
+# print(f"my name is {name} and my age is {age}")
+
+age = input("hello what is your age")
+print(age)
