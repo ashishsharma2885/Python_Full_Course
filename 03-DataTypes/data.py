2318aaba4672ab9b20e7876dfdcff9f75c73018b
ashishsharma2885 <ashishsharma60141@gmail.com>
2026-09-25 09:13:25 +0530

    learning today data type

---
+++ 03-DataTypes/data.py
@@ -0,0 +1,20 @@
+a = 12 #integer data type
+b = 30.9 #float data type
+c = 40/3 # also float data type
+v = 34j # complex data type
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(v))
+
+
+# learn here string data type
+st = '212321341234 didisisi @#!%'
+print(type(st))
+
+# boolean data learn
+z = True
+
+t = False
+print(type(z))
+
