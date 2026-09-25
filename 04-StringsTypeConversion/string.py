ab70b57fac4fad86f39b74441d19d8acc83445f5
ashishsharma2885 <ashishsharma60141@gmail.com>
2026-09-25 09:50:54 +0530

    learn string type conversion

---
+++ 04-StringsTypeConversion/string.py
@@ -0,0 +1,54 @@
+# string and type conversion
+
+a = "A"
+b = 65
+
+print(ord(a))
+print(chr(b))
+
+
+# indexing
+
+a = "ASHISH"
+
+print(a[0])
+print(a[1])
+print(a[2])
+print(a[3])
+print(a[4])
+print(a[5])
+
+print(a[-1])
+print(a[-2])
+print(a[-3])
+print(a[-4])
+print(a[-5])
+print(a[-6])
+
+
+# length of string
+
+print(len(a))
+
+
+# type conversion
+
+a = 12
+a = str(a)
+print(a)
+print(type(a))
+
+b = "13"
+b = int(b)
+print(b)
+print(type(b))
+
+c = 10
+c = float(c)
+print(c)
+print(type(c))
+
+d = 10.5
+d = int(d)
+print(d)
+print(type(d))
